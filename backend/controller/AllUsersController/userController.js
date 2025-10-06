c89170b5635c0fd1c7defa902ccb89973e11950a
RootUser-Ganga7702 <gangadharalothula7702@gmail.com>
2025-10-06 11:28:00 +0530

    get all users

--- backend/controller/AllUsersController/userController.js
+++ backend/controller/AllUsersController/userController.js
@@ -252,14 +252,14 @@ exports.userLogin = async (req, res) => {
 //   }
 // }
 
-// exports.getAllUsers = async (req, res) => {
-//   try {
-//     const users = await User.find();
-//     res.status(200).json({ responseCode: 200, message: "Users fetched successfully", users });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// }
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ responseCode: 200, message: "Users fetched successfully", users });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
 
 // exports.userStatusUpdate = async (req, res) => {
 //   try {
--- backend/routes/allUsersRoutes.js
+++ backend/routes/allUsersRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const allUsersRoutes = express.Router();
 
 const { registerAdmin, adminLogin } = require('../controller/AllUsersController/adminController');
-const { registerUser, verifyUser, resendOtp, userLogin } = require('../controller/AllUsersController/userController');
+const { registerUser, verifyUser, resendOtp, userLogin, getAllUsers } = require('../controller/AllUsersController/userController');
 
 allUsersRoutes.route('/registerAdmin').post(registerAdmin);
 allUsersRoutes.route('/adminLogin').post(adminLogin);
@@ -11,6 +11,7 @@ allUsersRoutes.route('/registerUser').post(registerUser);
 allUsersRoutes.route('/verifyUser').post(verifyUser);
 allUsersRoutes.route('/resendOtp').post(resendOtp);
 allUsersRoutes.route('/userLogin').post(userLogin);
+allUsersRoutes.route('/getAllUsers').get(getAllUsers);
 
 
 module.exports = allUsersRoutes;
